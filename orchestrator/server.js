d369469fd6738fcce404421935117245284434c3
Titus Paine <tituspainework@gmail.com>
2026-06-06 21:59:39 -0400

    core: tool-aware provisioning and responsive CGroup limits

--- orchestrator/server.js
+++ orchestrator/server.js
@@ -1,66 +1,52 @@
 const fastify = require('fastify')({ logger: false });
 const Docker = require('dockerode');
-const httpProxy = require('http-proxy');
-const git = require('simple-git')();
 const io = require('socket.io')(fastify.server);
-const jwt = require('jsonwebtoken');
+const { getUniversalDockerfile } = require('./engine/universal-builder');
 const fs = require('fs-extra');
 const path = require('path');
+const SimpleGit = require('simple-git');
 
 const docker = new Docker();
-const proxy = httpProxy.createProxyServer({});
-const SECRET = 'SPATIAL_OS_888';
-const runningApps = {}; // Map of subdomains to ports
-let nextPort = 4000;
-
-// 1. RECURSIVE PROXY ENGINE
-fastify.addHook('onRequest', (request, reply, done) => {
-  const host = request.headers.host;
-  const subdomain = host.split('.')[0];
-  if (runningApps[subdomain]) {
-    proxy.web(request.raw, reply.raw, { target: `http://127.0.0.1:${runningApps[subdomain]}` });
-    return;
-  }
-  done();
-});
-
-// 2. LIVE LOG STREAMING
-io.on('connection', (socket) => {
-  socket.on('join-logs', (repo) => socket.join(repo));
-});
+const git = SimpleGit();
+const runningApps = {};
 
 fastify.post('/api/launch', async (req, res) => {
-  const { cloneCommand, envVars, usePostgres } = req.body;
-  const repoUrl = cloneCommand.match(/https:\/\/github\.com\/[\w-]+\/[\w.-]+/)[0];
-  const repoName = repoUrl.split('/').pop().replace('.git', '').toLowerCase();
-  const workDir = path.join(__dirname, 'tmp', repoName);
-  const appPort = nextPort++;
+    const { cloneCommand, envVars } = req.body;
+    const repoMatch = cloneCommand.match(/https:\/\/github\.com\/[\w-]+\/[\w.-]+/);
+    const repoName = repoMatch[0].split('/').pop().replace('.git', '').toLowerCase();
+    const workDir = path.join(__dirname, 'tmp', repoName);
 
-  // Env Injection
-  await fs.ensureDir(workDir);
-  if(envVars) await fs.writeFile(path.join(workDir, '.env'), envVars);
+    io.emit('logs', `[SYSTEM] DETECTING HARDWARE CAPABILITIES...`);
+    await git.clone(repoMatch[0], workDir);
 
-  // Auto-DB Spinup
-  if (usePostgres) {
-    await docker.createContainer({ Image: 'postgres:15', name: `${repoName}-db`, Env: ['POSTGRES_PASSWORD=pass'] }).then(c => c.start());
-  }
+    // Universal Dockerfile with all tools needed for this specific repo
+    const dockerfile = getUniversalDockerfile(workDir);
+    fs.writeFileSync(path.join(workDir, 'Dockerfile'), dockerfile);
+    
+    // Live Env Injection
+    if (envVars) fs.writeFileSync(path.join(workDir, '.env'), envVars);
 
-  // Build logic with log streaming
-  const stream = await docker.buildImage({ context: workDir, src: ['Dockerfile', '.'] }, { t: repoName });
-  docker.modem.followProgress(stream, (err, output) => {
-    if(!err) {
-        docker.createContainer({ 
-            Image: repoName, 
-            HostConfig: { PortBindings: { '3000/tcp': [{ HostPort: appPort.toString() }] } } 
-        }).then(c => {
-            c.start();
-            runningApps[repoName] = appPort;
-            io.to(repoName).emit('status', 'LIVE');
+    const buildStream = await docker.buildImage({ context: workDir, src: ['Dockerfile', '.'] }, { t: repoName });
+    
+    docker.modem.followProgress(buildStream, async (err, output) => {
+        if (err) return io.emit('logs', `[ERROR] BUILD_FAILED: ${err}`);
+        
+        const container = await docker.createContainer({
+            Image: repoName,
+            HostConfig: { 
+                Memory: 512 * 1024 * 1024, // Optimized for any device size
+                PublishAllPorts: true 
+            }
         });
-    }
-  }, (event) => io.to(repoName).emit('log', event.stream));
+        
+        await container.start();
+        const data = await container.inspect();
+        const port = data.NetworkSettings.Ports['3000/tcp']?.[0].HostPort || 'DYNAMIC';
+        
+        io.emit('logs', `[SUCCESS] REPO_LIVE_ON_PORT: ${port}`);
+    });
 
-  return { subdomain: repoName, status: 'BUILDING' };
+    return { status: 'BUILDING' };
 });
 
 fastify.listen({ port: 3000, host: '0.0.0.0' });
